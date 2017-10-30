15714fb6cb9534458520a95cb76af646c1c5669f
Julio Albelda <juliet.xxl@gmail.com>
2017-10-30 20:39:59 +0100

    Funcionalidad botón Salir

--- app/src/main/java/org/example/asteriodes/MainActivity.java
+++ app/src/main/java/org/example/asteriodes/MainActivity.java
@@ -4,18 +4,36 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bAcercaDe;
+    private Button bSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bAcercaDe = (Button) findViewById(R.id.button03);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanzarAcercaDe(null);
+            }
+        });
+
+        bSalir = (Button) findViewById(R.id.button04);
+        bSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void lanzarAcercaDe(View view){
         Intent i = new Intent(this, AcercaDeActivity.class);
         startActivity(i);
     }
-
 }
