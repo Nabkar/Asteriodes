fc35b9b106aeb27205ce4bdc565d2619c876f5fd
Julio Albelda <juliet.xxl@gmail.com>
2017-10-30 20:22:09 +0100

    Actividad AcercaDe

---
+++ app/src/main/java/org/example/asteriodes/AcercaDeActivity.java
@@ -0,0 +1,16 @@
+package org.example.asteriodes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Julio on 30/10/2017.
+ */
+
+public class AcercaDeActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
--- app/src/main/java/org/example/asteriodes/MainActivity.java
+++ app/src/main/java/org/example/asteriodes/MainActivity.java
@@ -1,7 +1,9 @@
 package org.example.asteriodes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDeActivity.class);
+        startActivity(i);
+    }
+
 }
